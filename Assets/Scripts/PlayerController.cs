1d25c0e75720a4ee92d87aa1f04284b8d822db6a
Rahul Patwari <rahulpatwari233@gmail.com>
2018-11-30 23:43:39 +0530

    2/8: Added movement to player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // update is called before rendering the frame
+
+    // fixed update is called before performing any physics operation. 
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * 30);
+    }
+}
