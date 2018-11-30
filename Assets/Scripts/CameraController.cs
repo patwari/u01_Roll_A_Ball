8f75909d20ad3d6813b6a0b9715654a8abe5a41d
Rahul Patwari <rahulpatwari233@gmail.com>
2018-12-01 00:01:01 +0530

    3/10: Moving the camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once all the values are processed, and we're ready to render
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
